1b5c5442c3bc2f3504c8a4c32e9af25d06b96948
ivanrivas <ivan.rivas@cetys.edu.mx>
2013-10-29 15:32:25 -0700

    Upload

---
+++ Exceptions/CustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class CustomException:Exception
+    {
+        public string ErrorCode = "Error en formato tiene que escribir un int\n";
+        public CustomException()
+        {
+            Console.WriteLine(ErrorCode);
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Escribe tu edad\n");
+                    int edad = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Correcto!\n");
+                }
+                catch
+                {
+                    CustomException nulo = new CustomException();
+                }
+            }
+            while (true);
+        }
+    }
+}
